fb9479fe1ac58cdb3072048b505862472571b30d
Corey Black <coreyblack@hotmail.co.uk>
2025-11-04 20:52:14 +0000

    Update App.tsx
    -added leaderboard and login page routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import Register from "./pages/Register"
+import Leaderboard from "./pages/Leaderboard"
+import Login from "./pages/Login"
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Layout>
   )
