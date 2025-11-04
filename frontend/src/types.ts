bedbe92e5d75a0c7857206a0fac6342cf6f47957
Corey Black <coreyblack@hotmail.co.uk>
2025-11-04 17:51:56 +0000

    Create types.ts

---
+++ frontend/src/types.ts
@@ -0,0 +1,7 @@
+export type Event = { _id: string; name: string; date: string; location: string }
+export type Registration = {
+  firstName: string
+  lastName: string
+  email: string
+  eventId: string
+}
