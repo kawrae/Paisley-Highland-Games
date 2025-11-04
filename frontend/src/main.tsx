56f0768e851f324151bc54afe80c5da4042cd561
Corey Black <coreyblack@hotmail.co.uk>
2025-11-04 20:52:36 +0000

    Update main.tsx
    -Added authprovider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
 import App from "./App"
+import { BrowserRouter } from "react-router-dom"
+import { AuthProvider } from "./lib/auth"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
