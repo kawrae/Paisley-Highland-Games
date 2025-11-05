6261aa1d387eb55d008bf87e979438bf0f770692
Corey Black <coreyblack@hotmail.co.uk>
2025-11-05 02:27:58 +0000

    Update App.tsx
    -added AdminRegistrations page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import AdminRegistrations from "./pages/AdminRegistrations";
 import Leaderboard from "./pages/Leaderboard";
 import Login from "./pages/Login";
 
@@ -15,6 +16,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/admin/registrations" element={<AdminRegistrations />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/login" element={<Login />} />
         </Routes>
